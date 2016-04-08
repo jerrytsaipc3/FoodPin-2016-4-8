5146ea9647dd389413cfdbfe870595aa8716546d
jerrytsaipc3 <r02b44020@ntu.edu.tw>
2016-04-08 15:47:55 +0800

    CH15

---
+++ FoodPin/Restaurant.swift
@@ -0,0 +1,30 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by Simon Ng on 20/8/15.
+//  Copyright © 2015 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+class Restaurant {
+    var name = ""
+    var type = ""
+    var location = ""
+    var image = ""
+    var phone = ""
+    var isVisited = false
+    
+    
+    init(name:String, type:String, location:String, image:String, phone:String, isVisited:Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.image = image
+        self.phone = phone
+        self.isVisited = isVisited
+       
+    }
+}
+
---
+++ FoodPin/RestaurantDetailViewController.swift
@@ -0,0 +1,102 @@
+//
+//  RestaurantDetailViewController.swift
+//  FoodPin
+//
+//  Created by Simon Ng on 19/8/15.
+//  Copyright © 2015 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailViewController: UIViewController, UITableViewDataSource,
+      UITableViewDelegate {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! RestaurantDetailTableViewCell
+        
+        //set cell backgroundcolor, let background become transparent
+        cell.backgroundColor = UIColor.clearColor()
+        
+        //SET CELL
+        switch indexPath.row{
+        
+        case 0:
+            cell.fieldLabel.text = "Name"
+            cell.valueLabel.text = restaurant.name
+        case 1:
+            cell.fieldLabel.text = "Type"
+            cell.valueLabel.text = restaurant.type
+        case 2:
+            cell.fieldLabel.text = "Location"
+            cell.valueLabel.text = restaurant.location
+        case 3:
+            cell.fieldLabel.text = "Phone"
+            cell.valueLabel.text = restaurant.phone
+        case 4:
+            cell.fieldLabel.text = "Been here"
+            cell.valueLabel.text = (restaurant.isVisited) ? "Yes, I`ve been here before" : "No"
+        default:
+            cell.fieldLabel.text = ""
+            cell.valueLabel.text = ""
+            
+        }
+        return cell
+        
+    }
+    
+    @IBOutlet var restaurantImageView:UIImageView!
+    
+    var restaurant:Restaurant!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //set background color
+        restaurantImageView.image = UIImage(named: restaurant.image)
+        tableView.backgroundColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue:240.0/255.0, alpha:0.2)
+        
+        //cut surplus cell line
+        tableView.tableFooterView = UIView(frame: CGRectZero)
+        
+        //set line color
+        tableView.separatorColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue:240.0/255.0, alpha:
+            0.8)
+        
+        //title = restaurant name
+        title = restaurant.name
+        
+        //設定cell自動配合文字多寡調整高度
+        tableView.estimatedRowHeight = 36.0
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBOutlet var tableView:UITableView!
+    
+    //hide navigationbar in restaurantDetailView but appear on restaurantView
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.hidesBarsOnSwipe = false
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    
+}
