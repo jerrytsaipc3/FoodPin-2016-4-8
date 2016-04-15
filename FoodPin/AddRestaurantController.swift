5e0fa1a443d5948f394e2719c55cb7317b360b13
jerrytsaipc3 <r02b44020@ntu.edu.tw>
2016-04-15 16:57:33 +0800

    ch18.5
    photo fit

--- FoodPin/AddRestaurantController.swift
+++ FoodPin/AddRestaurantController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class AddRestaurantController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    //連結照片庫圖片
+    @IBOutlet var imageView:UIImageView!
+
+
     //偵測觸控與載入照片庫
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
     
@@ -29,27 +33,27 @@ class AddRestaurantController: UITableViewController, UIImagePickerControllerDel
     
     //此方法可以取得被選取的照片
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
         imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
-        imageView.contentMode = UIViewContentMode.ScaleToFill
+        imageView.contentMode = UIViewContentMode.ScaleAspectFill
         imageView.clipsToBounds = true
         
+        
         //將回傳圖片利用程式碼控制並做好比例調整
-        let leadingConstraint = NSLayoutConstraint(item: imageView, attribute:
-            NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: 0)
-            leadingConstraint.active = true
+        let leadingConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: 0)
+        leadingConstraint.active = true
         
         let trailingConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: 0)
-            trailingConstraint.active = true
+        trailingConstraint.active = true
         
         let topConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 0)
         topConstraint.active = true
         
         let bottomConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: 0)
         bottomConstraint.active = true
-
+        
         
         dismissViewControllerAnimated(true, completion: nil)
     }
-    //連結照片庫圖片
-    @IBOutlet var imageView:UIImageView!
 }
+   
