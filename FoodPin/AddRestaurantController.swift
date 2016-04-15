0658dbff8ab4cf1d7e375a2cc4a3ea4825c7e7c0
jerrytsaipc3 <r02b44020@ntu.edu.tw>
2016-04-15 13:52:09 +0800

    ch18.4 add new restaurant image

--- FoodPin/AddRestaurantController.swift
+++ FoodPin/AddRestaurantController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AddRestaurantController: UITableViewController {
+class AddRestaurantController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //偵測觸控與載入照片庫
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
@@ -19,11 +19,22 @@ class AddRestaurantController: UITableViewController {
             let imagePicker = UIImagePickerController()
                 imagePicker.allowsEditing = false
                 imagePicker.sourceType = .PhotoLibrary
+                imagePicker.delegate = self
                 
                 self.presentViewController(imagePicker, animated: true, completion: nil)
             }
         }
     tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
-
+    
+    //此方法可以取得被選取的照片
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        imageView.contentMode = UIViewContentMode.ScaleToFill
+        imageView.clipsToBounds = true
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    //連結照片庫圖片
+    @IBOutlet var imageView:UIImageView!
 }
